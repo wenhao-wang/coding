b5c30dbdb20eed3114cb9dbe2847764c020928f8
wangwhf <wangwhf@yonyou.com>
2018-12-14 00:13:10 +0800

    快排算法patition函数思想的引申使用

---
+++ com/company/Offer/Algorithom/$39Partition.java
@@ -0,0 +1,52 @@
+package com.company.Offer.Algorithom;/*
+ * @description
+ * 快速排序算法中Partition函数的思想，在一些场景中可以引用到其他一些查找中，且具有很好效率时间复杂度
+ * @author : wenhao
+ * @create : 2018/12/14 上午12:00
+ */
+
+public class $39Partition {
+    /*
+    * @description:
+    * 查找数组中出现次数超过一半的数字
+    * @author: ${USER}
+    * @create: ${DATE} ${TIME}
+    */
+    public int moreThanHalfNum(int[] num){
+        int len = num.length;
+
+        int middle = len >>2;
+        int end = len-1;
+        int start =0;
+        int patition = getPartition(num,start,end);
+        while (patition!= middle){
+            if(patition>middle){
+                end = patition-1;
+                patition = getPartition(num,start,end);
+            }else {
+                start = patition+1;
+                patition = getPartition(num,start,end);
+            }
+        }
+        return num[patition];
+    }
+
+    private int getPartition(int[] num, int start, int end) {
+        int base = start;
+        int index = start+1;
+        for (int i = index;i<=end;i++){
+            if(num[i]<num[base]){
+                swap(num,i,index++);
+            }
+        }
+        swap(num,base,index-1);
+        return index-1;
+    }
+
+    private void swap(int[] num, int i, int i1) {
+        int temp = num[i];
+        num[i] = num[i1];
+        num[i1] = temp;
+    }
+
+}
